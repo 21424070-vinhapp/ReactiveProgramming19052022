6de8743a435f69be6bb6cd2b660a06cbfdae21d4
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-19 16:50:29 +0700

    finish Synchornnous ve single item (generic) & multiple item (iterable)

--- app/src/main/java/com/example/reactiveprogramming19052022/MainActivity.java
+++ app/src/main/java/com/example/reactiveprogramming19052022/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.reactiveprogramming19052022;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //tao ra mang arr
+        List<String> arr=new ArrayList<>(Arrays.asList("Ty","Teo","tun"));
+        //tao ra bien iterable
+        Iterable<String> data= handle(arr);
+        //iterrator la otem cua iterable
+        Iterator<String> value=data.iterator();
+
+        //de hien ra du lieu cua iterator su dung vong lap while
+        while (value.hasNext())
+        {
+            Log.d("BBB",value.next());
+        }
+
     }
+
+    private Iterable<String> handle(List<String> arr) {
+        return arr;
+    }
+
+
 }
