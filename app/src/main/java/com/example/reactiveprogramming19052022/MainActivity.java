af0a1e5bf34127fea0908f11083cebf3b8816484
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-20 14:30:58 +0700

    vi du ve completetablefuture

--- app/src/main/java/com/example/reactiveprogramming19052022/MainActivity.java
+++ app/src/main/java/com/example/reactiveprogramming19052022/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.reactiveprogramming19052022;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -17,9 +21,11 @@ public class MainActivity extends AppCompatActivity {
     //ten tag trong logd
     static final String TAG = "BBB";
     //so luong thread duoc tao ra trong thread pool
+    static final int DEFAULT_THREAD_POOL_SIZE_COMPLETETABLE_FUTURE = 1;
     static final int DEFAULT_THREAD_POOL_SIZE_SUBMIT = 2;
     static final int DEFAULT_THREAD_POOL_SIZE_EXECUTE = 2;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,11 +44,21 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
         //submit
-        generateStringUseSubmit();
+        //generateStringUseSubmit();
 
         //execute
         //generateStringUseExecute();
 
+        //CompleteTable Future
+        try {
+            Log.d(TAG, "Ket qua \n "+ generateStringUseCompleteTableFuture().get());
+        } catch (ExecutionException e) {
+            Log.d(TAG, "Loi: "+e.getMessage());
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            Log.d(TAG, "Loi: "+e.getMessage());
+            e.printStackTrace();
+        }
 
     }
 
@@ -77,19 +93,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    private Future<String> generateStringUseSubmit1() {
-//        ExecutorService executorService = Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE_SUBMIT);
-//
-//        Future<String> future = executorService.submit(new Callable<String>() {
-//            @Override
-//            public String call() throws Exception {
-//                Log.d(TAG, Thread.currentThread().getName());
-//                Thread.sleep(1000);
-//                return "xyz";
-//            }
-//        });
-//        return future;
-//    }
+    private Future<String> generateStringUseSubmit1() {
+        ExecutorService executorService = Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE_SUBMIT);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Log.d(TAG, Thread.currentThread().getName());
+                Thread.sleep(1000);
+                return "xyz";
+            }
+        });
+        return future;
+    }
 
     private void generateStringUseExecute() {
         ExecutorService executorService = Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE_EXECUTE);
@@ -112,5 +128,31 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "run 2: " + Thread.currentThread().getName());
             }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private CompletableFuture<String> generateStringUseCompleteTableFuture()
+    {
+        ExecutorService executorService=Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE_COMPLETETABLE_FUTURE);
+
+        CompletableFuture<String> stringCompletableFuture=new CompletableFuture<>();
+
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Random random=new Random();
+                if(random.nextBoolean())
+                {
+                    stringCompletableFuture.complete("Ket qua: data");
+                }
+                else
+                {
+                    stringCompletableFuture.completeExceptionally(new Throwable("Loi gi do"));
+                }
+            }
+        });
+
+
+        return stringCompletableFuture;
     }
 }
